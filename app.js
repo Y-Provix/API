66510f39cbf070220894a31ae30dcb50b5750ee1
Provix <ygworkweb.5860@gmail.com>
2024-04-20 20:30:50 +0200

    saved

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 const express = require("express");
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-async function getReq(url) {
+async function getRequest(url) {
     try {
         const response = await axios.get(url);
         return response.data;
@@ -12,15 +12,39 @@ async function getReq(url) {
         throw error;
     }
 }
+
+async function postRequest(url, body) {
+    try {
+        const response = await axios.post(url, body);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 app.get("/", async (req, res) => {
-    res.send({ message: "Hello world :)" })
-})
-app.post("/getreq", async (req, res) => {
-    const jsonReturned = await getReq(req.body.url)
-    res.send(jsonReturned);
-})
+    res.send({ message: "Hello world :)" });
+});
 
+app.post("/get-request", async (req, res) => {
+    try {
+        const jsonReturned = await getRequest(req.body.url);
+        res.send(jsonReturned);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
 
-app.listen(3000 || process.env.PORT, () => {
-    console.log("Server is working!");
-})
+app.post("/post-request", async (req, res) => {
+    try {
+        const jsonReturned = await postRequest(req.body.url, req.body.body);
+        res.send(jsonReturned);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
